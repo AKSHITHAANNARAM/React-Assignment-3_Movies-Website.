39df0aa5a848f493abb6fa6ae541f3e16a0aac12
AKSHITHAANNARAM <113237537+AKSHITHAANNARAM@users.noreply.github.com>
2023-09-06 21:39:58 +0530

    App.js

--- movie-website/src/App.js
+++ movie-website/src/App.js
@@ -7,166 +7,172 @@ class App extends Component {
     this.state = {
       movies: [],
       query: '', // Add a state for the search query
-      errorMessage: '', // Add a state for the error message
-      movieQuotes: [  // Add a list of movie quotes
+      movieQuotes: [
+        // ... (movie quotes)
         {
-          quote: "May the Force be with you.",
-          movie: "Star Wars",
-          character: "Various",
-        },
+            quote: "May the Force be with you.",
+            movie: "Star Wars",
+            character: "Various",
+          },
+
         {
-          quote: "Here's looking at you, kid.",
-          movie: "Casablanca",
-          character: "Rick Blaine",
-        },
-        {
-            quote: "You can't handle the truth!",
-            movie: "A Few Good Men",
-            character: "Colonel Jessup",
+            quote: "Here's looking at you, kid.",
+            movie: "Casablanca",
+            character: "Rick Blaine",
           },
           {
-            quote: "Life is like a box of chocolates.",
-            movie: "Forrest Gump",
-            character: "Forrest Gump",
-          },
-          {
-            quote: "There's no place like home.",
-            movie: "The Wizard of Oz",
-            character: "Dorothy",
-          },
-          {
-            quote: "To infinity and beyond!",
-            movie: "Toy Story",
-            character: "Buzz Lightyear",
-          },
-          {
-            quote: "Here's Johnny!",
-            movie: "The Shining",
-            character: "Jack Torrance",
-          },
-          {
-            quote: "I'll be back.",
-            movie: "The Terminator",
-            character: "The Terminator",
-          },
-          {
-            quote: "You talking to me?",
-            movie: "Taxi Driver",
-            character: "Travis Bickle",
-          },
-          {
-            quote: "Bond. James Bond.",
-            movie: "Dr. No",
-            character: "James Bond",
-          },
-          {
-            quote: "Here's my ticket. Don't tear it.",
-            movie: "Willy Wonka & the Chocolate Factory",
-            character: "Willy Wonka",
-          },
-          {
-            quote: "May the odds be ever in your favor.",
-            movie: "The Hunger Games",
-            character: "Effie Trinket",
-          },
-          {
-            quote: "Why so serious?",
-            movie: "The Dark Knight",
-            character: "The Joker",
-          },
-          {
-            quote: "Keep your friends close, but your enemies closer.",
-            movie: "The Godfather Part II",
-            character: "Michael Corleone",
-          },
-          {
-            quote: "You can't handle my undivided attention.",
-            movie: "The Social Network",
-            character: "Mark Zuckerberg",
-          },
-          {
-            quote: "I feel the need... the need for speed.",
-            movie: "Top Gun",
-            character: "Maverick",
-          },
-          {
-            quote: "You can't handle the truth!",
-            movie: "A Few Good Men",
-            character: "Colonel Jessup",
-          },
-          {
-            quote: "Life is like a box of chocolates.",
-            movie: "Forrest Gump",
-            character: "Forrest Gump",
-          },
-          {
-            quote: "There's no place like home.",
-            movie: "The Wizard of Oz",
-            character: "Dorothy",
-          },
-          {
-            quote: "To infinity and beyond!",
-            movie: "Toy Story",
-            character: "Buzz Lightyear",
-          },
-          {
-            quote: "Here's Johnny!",
-            movie: "The Shining",
-            character: "Jack Torrance",
-          },
-          {
-            quote: "I'll be back.",
-            movie: "The Terminator",
-            character: "The Terminator",
-          },
-          {
-            quote: "You talking to me?",
-            movie: "Taxi Driver",
-            character: "Travis Bickle",
-          },
-          {
-            quote: "Bond. James Bond.",
-            movie: "Dr. No",
-            character: "James Bond",
-          },
-          {
-            quote: "Here's my ticket. Don't tear it.",
-            movie: "Willy Wonka & the Chocolate Factory",
-            character: "Willy Wonka",
-          },
-          {
-            quote: "May the odds be ever in your favor.",
-            movie: "The Hunger Games",
-            character: "Effie Trinket",
-          },
-          {
-            quote: "Why so serious?",
-            movie: "The Dark Knight",
-            character: "The Joker",
-          },
-          {
-            quote: "Keep your friends close, but your enemies closer.",
-            movie: "The Godfather Part II",
-            character: "Michael Corleone",
-          },
-          {
-            quote: "You can't handle my undivided attention.",
-            movie: "The Social Network",
-            character: "Mark Zuckerberg",
-          },
-          {
-            quote: "I feel the need... the need for speed.",
-            movie: "Top Gun",
-            character: "Maverick",
-          },
-        ],
-      };
-// intialize debouncedSearch in the constructor
+              quote: "You can't handle the truth!",
+              movie: "A Few Good Men",
+              character: "Colonel Jessup",
+            },
+            {
+              quote: "Life is like a box of chocolates.",
+              movie: "Forrest Gump",
+              character: "Forrest Gump",
+            },
+            {
+              quote: "There's no place like home.",
+              movie: "The Wizard of Oz",
+              character: "Dorothy",
+            },
+            {
+              quote: "To infinity and beyond!",
+              movie: "Toy Story",
+              character: "Buzz Lightyear",
+            },
+            {
+              quote: "Here's Johnny!",
+              movie: "The Shining",
+              character: "Jack Torrance",
+            },
+            {
+              quote: "I'll be back.",
+              movie: "The Terminator",
+              character: "The Terminator",
+            },
+            {
+              quote: "You talking to me?",
+              movie: "Taxi Driver",
+              character: "Travis Bickle",
+            },
+            {
+              quote: "Bond. James Bond.",
+              movie: "Dr. No",
+              character: "James Bond",
+            },
+            {
+              quote: "Here's my ticket. Don't tear it.",
+              movie: "Willy Wonka & the Chocolate Factory",
+              character: "Willy Wonka",
+            },
+            {
+              quote: "May the odds be ever in your favor.",
+              movie: "The Hunger Games",
+              character: "Effie Trinket",
+            },
+            {
+              quote: "Why so serious?",
+              movie: "The Dark Knight",
+              character: "The Joker",
+            },
+            {
+              quote: "Keep your friends close, but your enemies closer.",
+              movie: "The Godfather Part II",
+              character: "Michael Corleone",
+            },
+            {
+              quote: "You can't handle my undivided attention.",
+              movie: "The Social Network",
+              character: "Mark Zuckerberg",
+            },
+            {
+              quote: "I feel the need... the need for speed.",
+              movie: "Top Gun",
+              character: "Maverick",
+            },
+            {
+              quote: "You can't handle the truth!",
+              movie: "A Few Good Men",
+              character: "Colonel Jessup",
+            },
+            {
+              quote: "Life is like a box of chocolates.",
+              movie: "Forrest Gump",
+              character: "Forrest Gump",
+            },
+            {
+              quote: "There's no place like home.",
+              movie: "The Wizard of Oz",
+              character: "Dorothy",
+            },
+            {
+              quote: "To infinity and beyond!",
+              movie: "Toy Story",
+              character: "Buzz Lightyear",
+            },
+            {
+              quote: "Here's Johnny!",
+              movie: "The Shining",
+              character: "Jack Torrance",
+            },
+            {
+              quote: "I'll be back.",
+              movie: "The Terminator",
+              character: "The Terminator",
+            },
+            {
+              quote: "You talking to me?",
+              movie: "Taxi Driver",
+              character: "Travis Bickle",
+            },
+            {
+              quote: "Bond. James Bond.",
+              movie: "Dr. No",
+              character: "James Bond",
+            },
+            {
+              quote: "Here's my ticket. Don't tear it.",
+              movie: "Willy Wonka & the Chocolate Factory",
+              character: "Willy Wonka",
+            },
+            {
+              quote: "May the odds be ever in your favor.",
+              movie: "The Hunger Games",
+              character: "Effie Trinket",
+            },
+            {
+              quote: "Why so serious?",
+              movie: "The Dark Knight",
+              character: "The Joker",
+            },
+            {
+              quote: "Keep your friends close, but your enemies closer.",
+              movie: "The Godfather Part II",
+              character: "Michael Corleone",
+            },
+            {
+              quote: "You can't handle my undivided attention.",
+              movie: "The Social Network",
+              character: "Mark Zuckerberg",
+            },
+            {
+              quote: "I feel the need... the need for speed.",
+              movie: "Top Gun",
+              character: "Maverick",
+            },
+      ],
+      isSearchPerformed: false, // Track if a search has been performed
+    };
+    // Initialize debouncedSearch in the constructor
     this.debouncedSearch = this.debounce(this.handleSearch, 300); // Adjust delay as needed
-}
+  }
 
   componentDidMount() {
-    // Fetch data when the component mounts
+    // Fetch default movies when the component mounts
+    this.fetchDefaultMovies();
+  }
+
+  fetchDefaultMovies = () => {
     fetch('https://www.omdbapi.com/?apikey=45f0782a&s=war')
       .then((response) => response.json())
       .then((data) => {
@@ -175,7 +181,8 @@ class App extends Component {
         }
       })
       .catch((error) => console.error('Error fetching data:', error));
-  }
+  };
+
   debounce = (func, delay) => {
     let timer;
     return function () {
@@ -194,30 +201,30 @@ class App extends Component {
     this.debouncedSearch(query);
   };
 
-  debouncedSearch = this.debounce(this.handleSearch, 300); // Adjust delay as needed
-
   handleSearch = (query) => {
     if (query.trim() === '') {
-      this.setState({ movies: [], errorMessage: '' });
+      // If the query is empty, reset movies to defaultMovies
+      this.fetchDefaultMovies();
+      this.setState({ isSearchPerformed: false }); // No search has been performed
     } else {
       fetch(`https://www.omdbapi.com/?apikey=45f0782a&s=${query}`)
         .then((response) => response.json())
         .then((data) => {
           if (data.Search) {
-            this.setState({ movies: data.Search, errorMessage: '' });
+            this.setState({ movies: data.Search, isSearchPerformed: true });
           } else {
-            this.setState({ movies: [], errorMessage: 'No movies found.' });
+            this.setState({ movies: [], isSearchPerformed: true });
           }
         })
         .catch((error) => {
           console.error('Error fetching data:', error);
-          this.setState({ movies: [], errorMessage: 'An error occurred.' });
+          this.setState({ movies: [], isSearchPerformed: true });
         });
     }
   };
 
-render() {
-    const { movies, query, errorMessage, movieQuotes } = this.state;
+  render() {
+    const { movies, query, movieQuotes, isSearchPerformed } = this.state;
 
     return (
       <div className="App">
@@ -230,20 +237,23 @@ render() {
             placeholder="Search movies..."
             value={query}
             onChange={this.handleInputChange}
-            />
-            <button onClick={() => this.handleSearch(query)}>Search</button>
+          />
+          <button onClick={() => this.handleSearch(query)}>Search</button>
         </div>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
         <div className="movie-list">
-          {movies.map((movie) => (
-            <div className="movie-card" key={movie.imdbID}>
-              <img src={movie.Poster} alt={movie.Title} />
-              <div className="movie-info">
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
+          {isSearchPerformed && movies.length === 0 ? (
+            <p className="error-message">No movies found.</p>
+          ) : (
+            movies.map((movie) => (
+              <div className="movie-card" key={movie.imdbID}>
+                <img src={movie.Poster} alt={movie.Title} />
+                <div className="movie-info">
+                  <h3>{movie.Title}</h3>
+                  <p>{movie.Year}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
         <div className="movie-quotes-marquee">
           <h2>🎬 Famous Movie Quotes 🍿</h2>
@@ -266,3 +276,9 @@ render() {
 }
 
 export default App;
+
+
+
+
+
+
